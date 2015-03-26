fe5745f84b4aff99c5c6bcef91a58d1be6bcc93e
sami@addthis.com <sami@addthis.com>
2015-03-26 14:28:30 -0400

    more backbone stuff

---
+++ src/main/java/KonnichiwaApplication.java
@@ -0,0 +1,29 @@
+import io.dropwizard.Application;
+import io.dropwizard.setup.Bootstrap;
+import io.dropwizard.setup.Environment;
+
+/**
+ * Created by sami on 3/26/15.
+ */
+public class KonnichiwaApplication extends Application<KonnichiwaConfiguration> {
+    public static void main(String[] args) throws Exception {
+        new KonnichiwaApplication().run(args);
+    }
+
+    @Override
+    public String getName() {
+        return "hello-world";
+    }
+
+    @Override
+    public void initialize(Bootstrap<KonnichiwaConfiguration> bootstrap) {
+        // nothing to do yet
+    }
+
+    @Override
+    public void run(KonnichiwaConfiguration configuration,
+                    Environment environment) {
+        // nothing to do yet
+    }
+
+}
---
+++ src/main/java/KonnichiwaConfiguration.java
@@ -0,0 +1,7 @@
+import io.dropwizard.Configuration;
+
+/**
+ * Created by sami on 3/26/15.
+ */
+public class KonnichiwaConfiguration extends Configuration {
+}
