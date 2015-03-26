4e4b7f6a5c9afcd26802ca193b369e68807628ed
Michael Kitzman <mkitzman@yahoo.com>
2015-03-26 17:48:13 -0400

    remove console log

--- js/SlackThis.js
+++ js/SlackThis.js
@@ -78,7 +78,6 @@ var SlackThis = (function () {
 
                 for (key in members) {
                     if (members.hasOwnProperty(key)) {
-                        //console.log(members[key]);
                         user = {
                             id    : members[key].id,
                             name  : (members[key].real_name) ? members[key].real_name : members[key].name,
