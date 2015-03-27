68ef4f323a2b3e6a3828c3f400cec1a0198a8f50
sami@addthis.com <sami@addthis.com>
2015-03-26 22:09:54 -0400

    shuffling users

--- js/SlackThis.js
+++ js/SlackThis.js
@@ -147,12 +147,17 @@ var SlackThis = (function () {
                 }
             }
 
-            return userList;
+            return shuffle(userList);
 
         } else {
 
             return results.error
         }
+    },
+
+    shuffle = function(o){
+        for(var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+        return o;
     },
 
     get = function (url, query_params) {
